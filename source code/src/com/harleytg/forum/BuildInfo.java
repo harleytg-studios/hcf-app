92ff01b53fdb904800f9e1a6c9d30ac0c59bf0af
markhitchk <mark.hitchk@gmail.com>
2026-08-19 04:27:07 -0700

    Restore exact Beta v10000035 BuildInfo

--- source code/src/com/harleytg/forum/BuildInfo.java
+++ source code/src/com/harleytg/forum/BuildInfo.java
@@ -5,8 +5,8 @@ final class BuildInfo {
     // MAJOR.MINOR.PATCH-dev.N and GitHub tags add the leading "v".
     static final String VERSION = "1.0";
     static final String VERSION_TAG = "v1.0";
-    static final int VERSION_CODE = 10000055;
-    static final int INTERNAL_BUILD = 86;
+    static final int VERSION_CODE = 10000035;
+    static final int INTERNAL_BUILD = 88;
     static final String CHANNEL = "Dev";
     static final String BRAND = "Harley's Studio's";
     static final String SESSION_CLIENT = "Harley's Clan Forum App";
@@ -19,6 +19,7 @@ final class BuildInfo {
     // Future builds must remain monotonic for Android in-place updates.
     static final String VERSION_CODE_SCHEME = "major-release-v1";
 
+    static final boolean FIREBASE_WEB_CONFIG_BUNDLED = true;
     static final boolean FCM_CONFIGURED = false;
     static final String USER_AGENT_MARKER = "HarleysClanForumApp/1.0";
     static final String UPDATE_REPOSITORY = "markhitchk/hcf-app";
