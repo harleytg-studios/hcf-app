5891932e2eb9363e8c700a4946ab1f7ae7c2eb62
markhitchk <mark.hitchk@gmail.com>
2026-08-22 01:27:53 -0700

    Bump Dev build to v10000092

--- source code/src/com/harleytg/forum/BuildInfo.java
+++ source code/src/com/harleytg/forum/BuildInfo.java
@@ -3,7 +3,7 @@ package com.harleytg.forum.dev;
 /** Build identity for the Dev/Beta Harley's Clan Forum Android app. */
 final class BuildInfo {
     static final boolean ALLOW_UPDATE_CHANNEL_SWITCH = false;
-    static final String APK_FILE_NAME = "HCF-Beta-v10000091.apk";
+    static final String APK_FILE_NAME = "HCF-Beta-v10000092.apk";
     static final String BRAND = "Harley's Studios";
     static final String CHANNEL = "Dev";
     static final String DEFAULT_UPDATE_CHANNEL = "dev";
@@ -11,15 +11,15 @@ final class BuildInfo {
     static final boolean ENABLE_DEV_TEST_MENU = true;
     static final boolean FCM_CONFIGURED = false;
     static final boolean FIREBASE_WEB_CONFIG_BUNDLED = true;
-    static final int INTERNAL_BUILD = 111;
+    static final int INTERNAL_BUILD = 112;
     static final String META_LINE = "1.0 • Development / Beta";
     static final String SESSION_CLIENT = "Harley's Clan Forum App";
     static final String UPDATE_DEV_BRANCH = "dev";
     static final String UPDATE_REPOSITORY = "markhitchk/hcf-app";
     static final String UPDATE_STABLE_BRANCH = "stable";
-    static final String USER_AGENT_MARKER = "HarleysClanForumApp/1.0 Build/10000091";
+    static final String USER_AGENT_MARKER = "HarleysClanForumApp/1.0 Build/10000092";
     static final String VERSION = "1.0";
-    static final int VERSION_CODE = 10000091;
+    static final int VERSION_CODE = 10000092;
     static final String VERSION_BUILD_LINE = VERSION + " • Development / Beta • Build " + VERSION_CODE;
     static final String VERSION_CODE_SCHEME = "major-release-v1";
     static final String VERSION_TAG = "v1.0";
