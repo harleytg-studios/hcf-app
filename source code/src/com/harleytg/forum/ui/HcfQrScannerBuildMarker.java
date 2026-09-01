42b62a1733b5f83d806e193a4740b92cd3e09d48
markhitchk <mark.hitchk@gmail.com>
2026-09-01 05:03:00 -0700

    Expose native QR scanner marker to release gate

---
+++ source code/src/com/harleytg/forum/ui/HcfQrScannerBuildMarker.java
@@ -0,0 +1,7 @@
+package com.harleytg.forum.dev;
+
+/** Build-time marker proving the native ZXing HCF Authenticator scanner is packaged. */
+final class HcfQrScannerBuildMarker {
+    static final String VALUE = "HCF_AUTHENTICATOR_NATIVE_QR_SCANNER_V2";
+    private HcfQrScannerBuildMarker() {}
+}
